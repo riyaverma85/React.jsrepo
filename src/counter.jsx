b1d46862c69c72fc00ad90b5cb3555135205c333
riyaverma85 <riyaverma2976@gmail.com>
2025-07-23 01:12:29 -0700

    counter

--- src/counter.jsx
+++ src/counter.jsx
@@ -1,26 +1,46 @@
+// import { useState } from "react";
+// const Counter=()=>{
+//     const [count,setCount]=useState(0);
+
+//     return(
+//         <>
+          
+//           <div className="counter">
+//           <h2>{count}</h2>
+//           <button onClick={()=>{setCount(count+1)}} className="btn">Increment</button>
+//           <button onClick={()=>{setCount(count-1)}}className="btn1">Decrement</button>
+//           <button onClick={()=>setCount(0)} className="btn2">Reset</button>
+//           </div>
+//         </>
+//     )
+// }
+// export default Counter;
+
+//============================================APARTFUNCTION=====================================================================================================
+
 import { useState } from "react";
 const Counter=()=>{
     const [count,setCount]=useState(0);
-    //const increment=()=>{
-    //     setCount(count+1);
-    // }
-    // const decrement=()=>{
-    //     if(count==0)
-    //     {
-    //         alert("not less than 0!")
-    //     }
-    //     else{
-    //     setCount(count-1);
-    //     }
+    const increment=()=>{
+        setCount(count+1);
+    }
+    const decrement=()=>{
+        if(count==0)
+        {
+            alert("not less than 0!")
+        }
+        else{
+        setCount(count-1);
+        }
         
-    // }
+    }
     return(
         <>
           
           <div className="counter">
           <h2>{count}</h2>
-          <button onClick={()=>{setCount(count+1)}} className="btn">Increment</button>
-          <button onClick={()=>{setCount(count-1)}}className="btn1">Decrement</button>
+          <button onClick={increment} className="btn">Increment</button>
+          <button onClick={decrement}className="btn1">Decrement</button>
           <button onClick={()=>setCount(0)} className="btn2">Reset</button>
           </div>
         </>

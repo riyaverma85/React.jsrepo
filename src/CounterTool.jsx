49aa1eabb7021654a98e479f809e62c6ef8671ef
riyaverma85 <riyaverma2976@gmail.com>
2025-07-23 01:30:06 -0700

    counter

--- src/CounterTool.jsx
+++ src/CounterTool.jsx
@@ -1,7 +1,15 @@
 
+import { useSelector,useDispatch } from "react-redux";
+import { increment,decrement } from "./CounterSlice";
 const CounterTool=()=>{
+    const data=useSelector((state)=>state.counter.count)
     return(
         <>
+        <div>
+            <h2>Counter App!!</h2>
+            <button onClick={increment()}>Increment</button>
+            <button onClick={decrement()}>Decrement</button>
+        </div>
         </>
     )
 }

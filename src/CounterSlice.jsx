bcf37742a379a4932460090ff04b157a0aa9a5ec
riyaverma85 <riyaverma2976@gmail.com>
2025-07-23 02:55:44 -0700

    counter

--- src/CounterSlice.jsx
+++ src/CounterSlice.jsx
@@ -1,3 +1,35 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const counterSlice=createSlice({
+//     name:"counter",
+//     initialState:{
+//         count:0
+        
+//     },
+//     reducers:{
+//         increment:(state)=>{
+//             state.count ++;
+              
+//         },
+//         decrement:(state)=>{
+//             if(state.count==0)
+//             {
+//                 alert("Not Less Than 0!")
+//             }
+//             else{
+//              state.count --;
+//             }
+            
+//         },
+//         reset:(state)=>{
+//             state.count=0;
+//         }
+
+//     }
+// })
+// export const{increment,decrement,reset}=counterSlice.actions;
+// export default counterSlice.reducer;
+
 import { createSlice } from "@reduxjs/toolkit";
 
 const counterSlice=createSlice({
